bf95e9ad28e72aacfd3a626121f91f503a167833
Asafe Felipe <azapphrz@users.noreply.github.com>
2020-06-03 19:29:31 -0300

    Atualização do código "bits"

--- Codes/bits.py
+++ Codes/bits.py
@@ -1,38 +1,41 @@
 from math import trunc                                # Importa uma funcao para pegar a parte inteira de numero
-prec = int(input("Quantas casas decimais de precisao? "))
-complemento = int(input(
-'''[1]Complemento simples
-[2]Complemento complexo
+
+Precisao = int(input('''
+[1] Precisão simples
+[2] Precisão dupla
 Escolha: '''))
 
-if (complemento == 1):
+if (Precisao == 1):
     x = 23
-elif (complemento == 2):
+elif (Precisao == 2):
     x = 52
 else:
-    complemento = int(input(''' Tente novamente!
-[1] Complemento simples
-[2] Complemento duplo
+    Precisao = int(input('''
+Tente novamente!
+[1] Precisão simples
+[2] Precisão dupla
 Escolha: '''))
 
 n = float(input("Numero: "))                            # Número que quer converter para binario
 cont = 0
 inteiro = bin(trunc(n))                                 # Pega binario correspondente da parte inteira do numero
 numBinDec = []                                          # Lista para guardar a parte decimal do numero
-decimal = round(n % 1, prec)                            # Pega a parte decimal do numero, com n casas decimais
+decimal = n % 1                                         # Pega a parte decimal do numero
 
 while True:                                             # Entra em "loop infinito"
     cont += 1                                           # contador para mantiçar caso entre em um loop
-    bit = round(decimal * 2, prec)                      # Multiplica por 2, e guarda o resultado com 6 casas decimais
+    bit = decimal * 2                                   # Multiplica por 2, e guarda o resultado
     numBinDec.append(trunc(bit))                        # Guarda na lista a parte inteira do numero
-    decimal = round(bit % 1, prec)                      # Pega a parte decimal do numero, com n casas decimais
+    decimal = bit % 1                                   # Pega a parte decimal do numero
     if (cont == x):                                     # Testa o contador é igual a complemnto
         break                                           # Sai do "loop infinito"
     elif (decimal == 0):                                # Testa o numero tem um binario simples
         for c in range(cont, x):                        
-            numBinDec.append(0)                         # Adicionar mais (complemento - contador) zeros
+            numBinDec.append(0)                         # Adicionar mais (precisão - contador) zeros
         break                                           # Sai do "loop infinito"
 
 print(f"O número é {inteiro[2:]}", end = " ")
 for p in numBinDec:
     print(f"{p}", end = "")
+    
+input("\nPressione ENTER para cintinuar...")
